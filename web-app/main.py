4443c8975557bbc019e9f3e6177e130fc8efc3d0
Connor Hornibrook <hornibrook@Connors-MacBook-Pro.local>
2020-04-28 10:34:35 -0400

    update

--- web-app/main.py
+++ web-app/main.py
@@ -31,11 +31,9 @@ def get_page_data(id, model, area_type):
     hour = db.session.query(CrimeHourClass.classification).filter(between(hour, CrimeHourClass.lower_bound, CrimeHourClass.upper_bound)).scalar()
     month = int(date.strftime("%m"))
     day_of_week = int(date.strftime("%w"))
-
-    if request.args.get("area-type") == "d":
-        area = request.args.get("district").split("/")
-    else:
-        area = request.args.get("neighborhood").split("/")
+    area_type = request.args.get("area-type")
+    uri = request.args.get("district" if area_type == "d" else "neighborhood")
+    area = uri.split("/")
     area = int(area[len(area) - 1])
 
     model_args = [[area, month, day_of_week, hour]]
@@ -43,7 +41,7 @@ def get_page_data(id, model, area_type):
     districts, neighborhoods = get_all_geoms()
     g = db.session.query(func.ST_Centroid(model.geom)).filter_by(id=id).scalar()
     if g is not None:
-        crime_model = db.session.query(CrimeModel).filter_by(area_type=area_type, classifier="MultinomialNB").first()
+        crime_model = db.session.query(CrimeModel).filter_by(area_type=area_type.upper(), classifier="MultinomialNB").first()
         crime_model = pickle.loads(crime_model.model)
         prediction = crime_model.predict(model_args)[0]
 
@@ -51,7 +49,7 @@ def get_page_data(id, model, area_type):
         geojson = json.loads(geojson)
         g = db.session.query(func.ST_X(g).label("x"), func.ST_Y(g).label("y")).first()
         x, y = g.x, g.y
-        return render_template("index.html", prediction=prediction, calendar=get_date_str(), x=x, y=y, geojson=geojson, districts=districts, neighborhoods=neighborhoods)
+        return render_template("index.html", selected=uri, area_type=area_type, prediction=prediction, calendar=date.strftime("%Y-%m-%dT%H:%M"), x=x, y=y, geojson=geojson, districts=districts, neighborhoods=neighborhoods)
     else:
         return make_response(f"<h2>ID {id} not found.", 404)
 
@@ -69,7 +67,7 @@ def neighborhood(id):
 @app.route("/")
 def index():
     districts, neighborhoods = get_all_geoms()
-    return render_template("index.html", x=_philly_x, y=_philly_y, calendar=get_date_str(), districts=districts, neighborhoods=neighborhoods)
+    return render_template("index.html", area_type="d", selected="", x=_philly_x, y=_philly_y, calendar=get_date_str(), districts=districts, neighborhoods=neighborhoods)
 
 
 if __name__ == "__main__":
